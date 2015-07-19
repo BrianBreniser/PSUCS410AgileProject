cbc5a980bfdd5277783019b7e2e3a0c5ef8821b3
brianbreniser <brianbreniser@gmail.com>
2015-07-19 12:31:37 -0700

    converted tabs to spaces in authentication.java

--- authentication.java
+++ authentication.java
@@ -10,48 +10,48 @@
 import java.io.*;
 import java.util.Scanner;
 import java.security.*;
-	
+
 
 public class authentication {
-	
-	private Scanner input = new Scanner(System.in);
-	private String user;
-	private String pass;
-	Console console = System.console();
-	
 
-	/* main() for testing purposes */
-	/*
-	public static void main(String [] args) {
-		System.out.println("Calling getCredentials()");
-		authentication cred = new authentication();
-		int ret = cred.getCredentials();
-	}
-	*/
-	
-	public String getUsername() {
-		
-		System.out.print("Username: ");
-		user = input.next();
-		return user;
-	}
-	
-	public String getPassword() {
+    private Scanner input = new Scanner(System.in);
+    private String user;
+    private String pass;
+    Console console = System.console();
 
-		try {
-	        char [] passwordChar = console.readPassword("Password: ");
-	        pass = new String(passwordChar);
-		}
-		catch (NullPointerException e) {
-			System.out.print("Password: ");
-	        pass = input.next();
-		}
 
-		return pass;
-	}
-	
-	private int hashPassword(String pw) {
-	
+    /* main() for testing purposes */
+    /*
+    public static void main(String [] args) {
+        System.out.println("Calling getCredentials()");
+        authentication cred = new authentication();
+        int ret = cred.getCredentials();
+    }
+    */
+
+    public String getUsername() {
+
+        System.out.print("Username: ");
+        user = input.next();
+        return user;
+    }
+
+    public String getPassword() {
+
+        try {
+            char [] passwordChar = console.readPassword("Password: ");
+            pass = new String(passwordChar);
+        }
+        catch (NullPointerException e) {
+            System.out.print("Password: ");
+            pass = input.next();
+        }
+
+        return pass;
+    }
+
+    private int hashPassword(String pw) {
+
         // Hash pw
         try {
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
@@ -59,8 +59,8 @@ public class authentication {
             String encryptedString = new String(messageDigest.digest());
         }
         catch (NoSuchAlgorithmException e) {
-        	System.err.println("Not a valid message digest algorithm");
-        	return -1;
+            System.err.println("Not a valid message digest algorithm");
+            return -1;
         }
 
         // Check database for user match (and if so password match)
@@ -69,7 +69,7 @@ public class authentication {
         
         // If user not in db, add to db
         
-		return 0;
-	}
-	
+        return 0;
+    }
+
 };
